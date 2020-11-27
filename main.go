afc2bdc4604ad08560f899a22dc0f8a65d040ff7
Ken Faulkner <Ken.Faulkner@gmail.com>
2020-11-28 10:06:18 +1100

    queues processing fine

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,13 +11,59 @@ import (
 type Server struct {
 }
 
+type InvokeRequest struct {
+	Data     map[string]interface{}
+	Metadata map[string]interface{}
+}
+
+type InvokeResponse struct {
+	Outputs     map[string]interface{}
+	Logs        []string
+	ReturnValue interface{}
+}
+
+
+func (s *Server) queueTriggerHandler(w http.ResponseWriter, r *http.Request) {
+	var invokeReq InvokeRequest
+	d := json.NewDecoder(r.Body)
+	decodeErr := d.Decode(&invokeReq)
+	if decodeErr != nil {
+		http.Error(w, decodeErr.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("The JSON data is:invokeReq metadata......")
+	fmt.Println(invokeReq.Metadata)
+	fmt.Println("The JSON data is:invokeReq data......")
+	fmt.Println(invokeReq.Data)
+
+	returnValue := fmt.Sprintf("HelloWorld: %s", invokeReq.Data["myQueueItem"])
+	invokeResponse := InvokeResponse{Logs: []string{"test log1", "test log2"}, ReturnValue: returnValue}
+
+	js, err := json.Marshal(invokeResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	//w.WriteHeader(200)
+}
+
 func (s *Server) simpleHttp(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got simple\n")
-	fmt.Fprintf(w, "XXXXXXXXXXX testing testing 1,2,3")
+	fmt.Fprintf(w, "afken2 example...")
+}
+
+func (s *Server) simpleHttp2(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got simple\n")
+	fmt.Fprintf(w, "afken2 example 2...")
 }
 
 func (s *Server) routes() {
 	http.HandleFunc("/testfunction", s.simpleHttp)
+	http.HandleFunc("/testfunction2", s.simpleHttp2)
+	http.HandleFunc("/queuetrigger", s.queueTriggerHandler)
 }
 
 func (s *Server) run() {
